ca329350730eed4f1f284f5669b38d6c6cc93044
ayshajaved <ayshajavd@gmail.com>
2024-07-01 14:45:45 +0500

    audiofilename changed

--- texttospeech.py
+++ texttospeech.py
@@ -3,6 +3,6 @@ from gtts import gTTS
 language = "en"                                             #selecting language
 text = "hey there! i am ayesha javed. Welcome all of you!"  #defining text
 speech = gTTS(text = text, lang = language)                  
-speech.save("lan.mp3")                                      #saving the speech as a mp3 file
+speech.save("speech.mp3")                                      #saving the speech as a mp3 file
 
 #after running a file will be created saving the mp3 file of the speech
